2782d0bbb7acf916fb0895030d061bcbc94159ad
Bryce Lynch <techbryce@hotmail.com>
2022-08-07 20:33:02 +1000

    UPDATE - Created MainpageHeader.xaml

---
+++ Bitspace/Bitspace/Pages/Mainpage/Controls/MainpageHeader.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Bitspace.Services.AnimationService;
+using Xamarin.Forms;
+
+namespace Bitspace.Pages.Mainpage.Controls;
+
+public partial class MainpageHeader
+{
+    private readonly IAnimationService _animationService;
+    public MainpageHeader()
+    {
+        InitializeComponent();
+        _animationService = new AnimationService();
+        AppIcon.Opacity = 0;
+
+        _ = StartAnimation();
+    }
+
+    private async Task StartAnimation()
+    {
+        await _animationService.ScaleIn(AppName);
+        await AnimateIcon();
+    }
+
+    private async Task HideIcon()
+    {
+        await AppIcon.TranslateTo(-AppIcon.Width, AppIcon.Height, 0);
+    }
+
+    private async Task AnimateIcon()
+    {
+        await HideIcon();
+        _ = _animationService.FadeIn(AppIcon, 250);
+        await AppIcon.TranslateTo(0, 0, 750, Easing.CubicInOut);
+    }
+}
