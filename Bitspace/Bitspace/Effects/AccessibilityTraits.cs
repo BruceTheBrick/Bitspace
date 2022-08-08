dac16f270362a0e085aabfc5842123c7e2df72ea
Bryce <techbryce@hotmail.com>
2022-08-09 09:54:28 +1000

    ADD - Began to implement AccessibilityTraits.cs and IAccessibility.cs

---
+++ Bitspace/Bitspace/Effects/AccessibilityTraits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Bitspace.Effects;
+
+public class AccessibilityTraits : RoutingEffect
+{
+    [Flags]
+    public enum Traits : long
+    {
+        None = 0,
+        Button = 1,
+        Link = 2,
+        Image = 4,
+        Selected = 8,
+        PlaysSound = 16, // 0x0000000000000010
+        KeyboardKey = 32, // 0x0000000000000020
+        StaticText = 64, // 0x0000000000000040
+        SummaryElement = 128, // 0x0000000000000080
+        NotEnabled = 256, // 0x0000000000000100
+        UpdatesFrequently = 512, // 0x0000000000000200
+        SearchField = 1024, // 0x0000000000000400
+        StartsMediaSession = 2048, // 0x0000000000000800
+        Adjustable = 4096, // 0x0000000000001000
+        AllowsDirectInteraction = 8192, // 0x0000000000002000
+        CausesPageTurn = 16384, // 0x0000000000004000
+        Header = 65536, // 0x0000000000010000
+    }
+
+
+
+    protected AccessibilityTraits()
+        : base(nameof(AccessibilityTraits))
+    {
+    }
+}
---
+++ Bitspace/Bitspace/Interfaces/IAccessibility.cs
@@ -0,0 +1,7 @@
+﻿namespace Bitspace.Interfaces;
+
+public interface IAccessibility
+{
+    public string Name { get; set; }
+    public string HelpText { get; set; }
+}
