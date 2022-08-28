f509a8e053ff5cc6ad830e6fba7af3fd0066547a
Bryce <techbryce@hotmail.com>
2022-08-28 21:48:00 +1000

    ADD - Tried to implement PillList directly into WeatherForecastPage.xaml

--- Bitspace/Bitspace/Controls/PillList.xaml.cs
+++ Bitspace/Bitspace/Controls/PillList.xaml.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Bitspace.Controls;
 
-[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class PillList : ContentPage
+[ExcludeFromCodeCoverage]
+public partial class PillList
 {
+    public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+        nameof(ItemsSource),
+        typeof(IList<Pill>),
+        typeof(PillList),
+        null,
+        BindingMode.TwoWay);
+
     public PillList()
     {
         InitializeComponent();
+    }
+
+    public IList<Pill> ItemsSource
+    {
+        get => (IList<Pill>)GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
     }
 }
