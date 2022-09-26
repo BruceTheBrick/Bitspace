97c5226b07a8901c9194708b459fb92211f17719
Bryce <techbryce@hotmail.com>
2022-09-26 20:48:58 +1000

    ADD - Created DateTimeExtensions.cs

---
+++ Bitspace/Bitspace/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bitspace.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToDisplayString(this DateTime datetime)
+        {
+            var dayName = datetime.ToString("dddd");
+            var shortMonth = datetime.ToString("MMM");
+            var date = datetime.Day;
+            return $"{dayName}, {date} {shortMonth}";
+        }
+    }
+}
