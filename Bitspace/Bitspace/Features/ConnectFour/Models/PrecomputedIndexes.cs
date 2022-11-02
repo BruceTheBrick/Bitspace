10397950824ceb801c818e5bc1ea426a2948b866
Bryce Lynch <techbryce@hotmail.com>
2022-11-02 22:03:44 +1100

    ADD - Began to create PrecomputedIndexes.cs

---
+++ Bitspace/Bitspace/Features/ConnectFour/Models/PrecomputedIndexes.cs
@@ -0,0 +1,18 @@
+namespace Bitspace.Features
+{
+    public static class PrecomputedIndexes
+    {
+        public static int[][] GetStandardPrecomputedIndexes()
+        {
+            var board = new int[6][];
+            board[0] = new[] { 1, 2, 5, 10, 5, 2, 1 };
+            board[1] = new[] { 2, 2, 5, 10, 5, 2, 2 };
+            board[2] = new[] { 5, 2, 5, 10, 5, 2, 5 };
+            board[3] = new[] { 5, 2, 5, 10, 5, 2, 5 };
+            board[4] = new[] { 2, 2, 5, 10, 5, 2, 2 };
+            board[5] = new[] { 1, 2, 5, 10, 5, 2, 1 };
+
+            return board;
+        }
+    }
+}
