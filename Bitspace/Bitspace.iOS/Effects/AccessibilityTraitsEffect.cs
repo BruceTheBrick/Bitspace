b0a1d34fbab504f88b4eba0917fe2f2d3cd0e1e4
Bryce Lynch <techbryce@hotmail.com>
2022-12-02 22:47:52 +1100

    ADD - Started to add AccessibilityTraits.cs

---
+++ Bitspace/Bitspace.iOS/Effects/AccessibilityTraitsEffect.cs
@@ -0,0 +1,31 @@
+﻿using Bitspace.Core;
+using Bitspace.Effects;
+using Bitspace.iOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(AccessibilityTraitsEffect), nameof(AccessibilityTraits))] 
+namespace Bitspace.iOS.Effects
+{
+    public class AccessibilityTraitsEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Element == null)
+            {
+                return;
+            }
+            
+            SetTraits();
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        private void SetTraits()
+        {
+            var traits = AccessibilityTraits.
+        }
+    }
+}
---
+++ Bitspace/Bitspace/Effects/AccessibilityTraits.cs
@@ -0,0 +1,25 @@
+﻿using Bitspace.Core;
+using Bitspace.Enums;
+using Xamarin.Forms;
+
+namespace Bitspace.Effects
+{
+    public class AccessibilityTraits : RoutingEffect
+    {
+        protected AccessibilityTraits()
+            : base(EffectHelper.GetLocalName<AccessibilityTraits>())
+        {
+
+        public static readonly BindableProperty TraitsProperty = BindableProperty.Create(
+            nameof(Traits),
+            typeof(TraitsEnum),
+            typeof(AccessibilityTraits));
+
+        public TraitsEnum Traits
+        {
+            get => (TraitsEnum)GetValue(TraitsProperty);
+            set => SetValue(TraitsProperty, value);
+        }
+    }
+}
+}
