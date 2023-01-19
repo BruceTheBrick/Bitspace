4efbe1a37c970271c9dffd5e7ee4ed1c234f24e5
Bryce Lynch <techbryce@hotmail.com>
2023-01-19 18:07:44 +1100

    UPDATE - Updated Playground pages and NavigationBar.xaml

--- Bitspace/Bitspace/Features/Playground/Buttons/ButtonsPlaygroundPage.xaml.cs
+++ Bitspace/Bitspace/Features/Playground/Buttons/ButtonsPlaygroundPage.xaml.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Bitspace.Features.Buttons
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ButtonsPlaygroundPage : ContentPage
+    [ExcludeFromCodeCoverage]
+    public partial class ButtonsPlaygroundPage
     {
         public ButtonsPlaygroundPage()
         {
